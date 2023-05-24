190e3c7239f4eb23e1e29c9c7f05731260db5778
Andrei Stezenko <andrew.stezenko@gmail.com>
2023-05-25 00:34:33 +0300

    feat: get images array with array_map.

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 
 $files	= scandir( 'images' );
-$images	= [];
-
-foreach( $files as $f ){
-	if( is_file( "images/$f" ) && preg_match( '/^.*\.(png|jpg|jpeg|gif|webp|avif|svg)$/i', $f ) ) $images[] = $f;
-}
-
-echo '<pre>' . print_r( $images, 1 ) . '</pre>';
+$images	= array_values(
+	array_filter( $files, function( $f ){
+		return ( is_file( "images/$f" ) && preg_match( '/^.*\.(png|jpg|jpeg|gif|webp|avif|svg)$/i', $f ) );
+	} )
+);
 
