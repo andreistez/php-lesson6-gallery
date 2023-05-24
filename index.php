f0ec3739a0f7c1a96eb40e586a7e5a50e54de8e3
Andrei Stezenko <andrew.stezenko@gmail.com>
2023-05-25 00:42:07 +0300

    feat: output images.

--- index.php
+++ index.php
@@ -6,4 +6,17 @@ $images	= array_values(
 		return ( is_file( "images/$f" ) && preg_match( '/^.*\.(png|jpg|jpeg|gif|webp|avif|svg)$/i', $f ) );
 	} )
 );
+?>
+
+<div class="gallery">
+	<?php
+	foreach( $images as $img ){
+		?>
+		<div class="gallery-item" style="width: 100px; margin-bottom: 10px">
+			<img style="width: 100%; height: auto" src="images/<?=$img?>" alt="<?php echo preg_replace( '/\.[^.]+$/', '', $img ) ?>" />
+		</div>
+		<?php
+	}
+	?>
+</div>
 
