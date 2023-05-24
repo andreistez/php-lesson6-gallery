6d08b449dfecd30900b8a1dfe3472b4d6fdd2138
Andrei Stezenko <andrew.stezenko@gmail.com>
2023-05-25 00:27:43 +0300

    feat: add images; get images array with foreach.

--- admin.php
+++ admin.php
@@ -1,6 +1,2 @@
 <?php
 
-$files = scandir( 'images' );
-
-echo '<pre>' . print_r( $files, 1 ) . '</pre>';
-
--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php
+
+$files	= scandir( 'images' );
+$images	= [];
+
+foreach( $files as $f ){
+	if( is_file( "images/$f" ) && preg_match( '/^.*\.(png|jpg|jpeg|gif|webp|avif|svg)$/i', $f ) ) $images[] = $f;
+}
+
+echo '<pre>' . print_r( $images, 1 ) . '</pre>';
+
